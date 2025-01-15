ad198e3d4872b950fcb9d32cba8b40c36660c50c
JayGangwar07 <jaygangwar86@gmail.com>
2025-01-15 15:42:48 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title='IPL_Score_Predictor',layout="centered")
 #Get the ML model 
 
 filename='model.pbz2'
-model = pickle.load(open(filename,'rb'))
+model = pickle.load(filename)
 
 #Title of the page with CSS
 
